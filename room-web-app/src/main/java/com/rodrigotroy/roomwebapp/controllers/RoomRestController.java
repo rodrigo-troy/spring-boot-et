c1bfb562edafbf8401a42cbfa7f7ed8663699f95
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2022-05-22 16:49:36 +0100

    Rest WS example

--- room-web-app/src/main/java/com/rodrigotroy/roomwebapp/controllers/RoomRestController.java
+++ room-web-app/src/main/java/com/rodrigotroy/roomwebapp/controllers/RoomRestController.java
@@ -1,7 +1,12 @@
 package com.rodrigotroy.roomwebapp.controllers;
 
+import com.rodrigotroy.roomwebapp.models.Room;
+import com.rodrigotroy.roomwebapp.services.RoomService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,4 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomRestController {
+    private final RoomService roomService;
+
+    public RoomRestController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public List<Room> getAllRooms() {
+        return roomService.getAllRooms();
+    }
 }
