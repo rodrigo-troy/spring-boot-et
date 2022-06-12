6de0245bc32a1dfc437c83ee8d3b0cf2fec1e215
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2022-06-12 18:40:33 +0100

    Basic Spring REST WS example

---
+++ simple-rest-ws-demo/src/main/java/com/rodrigotroy/spring/boot/simplerestdemo/SimpleRestWsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.rodrigotroy.spring.boot.simplerestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestWsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRestWsDemoApplication.class,
+                              args);
+    }
+
+}
---
+++ simple-rest-ws-demo/src/test/java/com/rodrigotroy/spring/boot/simplerestdemo/SimpleRestWsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigotroy.spring.boot.simplerestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestWsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
